a8e5b369421ef485fcd2749510c0826eb0cbcb77
ccrack <clebertcivil01@gmail.com>
2025-08-14 06:12:11 -0400

    Register endpoint that allow user to create new account

--- models/questions.js
+++ models/questions.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const questionSchema = new mongoose.Schema({
     title: String,
     body: String,
-    category: { type: String, required: true }, 
+    category: { type: String}, 
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     answers: [
         {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+
+// refference to models object
+const Questions = require('../models/questions');
+const Users = require('../models/users');
 
 
 // login
 router.get('/', (req, res) => {
-    res.send({data: "user login"});
+    res.send({ data: "user login" });
 });
 
 // Register
-router.post('/', (req, res) => {
-    res.send({data: "user register"});
+router.post('/', async (req, res) => {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = new Users(
+        {
+            username: req.body.username,
+            password: hashedPassword,
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName
+        });
+
+    await user.save();
+    res.json({ message: 'user registered' });
 });
 
 module.exports = router;
