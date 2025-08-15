cff6271ac66d4fcb73864bb80dc2ba124e78c371
ccrack <clebertcivil01@gmail.com>
2025-08-15 05:36:38 -0400

    create login route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,19 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // refference to models object
-const Questions = require('../models/questions');
+const Question = require('../models/questions');
 const Users = require('../models/users');
 
 
 // login
-router.get('/', (req, res) => {
-    res.send({ data: "user login" });
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await Users.findOne({ username: username });
+
+    if (!user) return res.status(400).json({ error: 'User not found' });
+    if (!await bcrypt.compare(password, user.password))
+        return res.status(400).json({ error: 'Wrong password' });
+
+    // create payload
+    const payload = {
+        id: user._id,
+        username: user.username
+    }
+
+    //sign token
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+    res.json({ message: "Login successful", token });
+
 });
 
+
 // Register
-router.post('/', async (req, res) => {
+router.post('/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const user = new Users(
         {
