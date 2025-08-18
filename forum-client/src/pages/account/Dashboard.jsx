925be62de1d106ab0a9b8b47be102f18a64c643d
ccrack <clebertcivil01@gmail.com>
2025-08-18 03:58:13 -0400

    create navbar in dashboard

--- forum-client/src/pages/account/Dashboard.jsx
+++ forum-client/src/pages/account/Dashboard.jsx
@@ -5,21 +5,40 @@ import { useNavigate } from 'react-router-dom';
 export default function Dashboard({ user, setUser }) {
     const navigate = useNavigate();
     const logout = () => {
-        // localStorage.removeItem('token');
+        localStorage.removeItem('token');
         // setUser(null);
         navigate('/login');// go to login
     };
     return (
-        <div className='container'>
-            <h1>Forum App</h1>
-            {user ? (
-                <div>
-                    <p>Welcome, {user.username}</p>
-                    <button onClick={logout}>Logout</button>
+        <div>
+
+            <nav className="navbar bg-dark navbar-expand-lg" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Forum app</a>
+                    <ul className="navbar-nav mb-lg-0">
+                        <li className="nav-item">
+                            {user ? (
+                                <div className='d-flex'>
+                                    <a href='#' className='nav-link'>Welcome, {user.username}</a>
+                                    <a href='' className='nav-link' onClick={logout}>Logout</a>
+                                </div>
+                            ) : (
+                                navigate('/login')
+                            )}
+                        </li>
+                    </ul>
                 </div>
-            ) : (
-                navigate('/login')
-            )}
+            </nav>
+
+
+            <div className='container-fluid'>
+
+                hello dashboard...
+
+            </div>
+
         </div>
+
+
     );
 }
